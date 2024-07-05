748ba80e137ac3ca06c7310c8907a6cf7703412f
Soumya Behera <lpsoumyaprakash@gmail.com>
2024-07-05 22:30:03 +0530

    first commit
    follow along application for websocket from spring guides

---
+++ src/test/java/com/websocket/mychat/MychatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.websocket.mychat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MychatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
